1255ea9f21b7c719540fb5270e9e0f622ece940a
darrin@darrinduncan.com <darrin@darrinduncan.com>
2025-12-27 21:35:45 -0500

    Add Temporary Category data

--- src/App.js
+++ src/App.js
@@ -1,43 +1,30 @@
-const App = () => {
-  return (
-    <div className='categories-container'>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-    </div>
-  );
-};
+import React from 'react'
 
-export default App;
+function App() {
+
+  const categories = [
+    { id: 1, title: "Hats", image:"" },
+    { id: 2, title: "Jackets", image:"" },
+    { id: 3, title: "Sneakers", image:"" },
+    { id: 4, title: "Women's", image:"" },
+    { id: 5, title: "Men's", image:"" },
+  ]
+
+  return (
+    <div classtitle='categories-container'>
+      {
+        categories.map((category) => (
+          <div key={category.id} classtitle='category-container'>
+            <div className='background-image' />
+            <div classtitle='category-body-container'>
+              <h2>{category.title}</h2>
+              <p>Shop Now</p>
+            </div>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
+
+export default App
